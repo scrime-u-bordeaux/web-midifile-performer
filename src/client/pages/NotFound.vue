d1c9d44bbd50050fbb34b5c34c0be9624ed272b8
rblard <rblard@enseirb-matmeca.fr>
2023-10-11 17:21:55 +0200

    Localization for Home, FirstSteps, LookForScores, NotFound and the Scroll Bar

--- src/client/pages/NotFound.vue
+++ src/client/pages/NotFound.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
-    Cette page n'existe pas ! :(
+  <div class="container">
+    <h2>
+      {{$t('notFound')}}
+    </h2>
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 25rem;
+}
+h2 {
+  color: #666;
+  font-style: italic;
+}
+</style>
 
 <script>
 export default {};
