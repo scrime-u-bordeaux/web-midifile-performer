2a9b8ea5ec7f3328309fc381aac507fbd3d1ee8c
rblard <rblard@enseirb-matmeca.fr>
2023-11-07 14:45:59 +0100

    Outsourced input logic to NumberInput component
    This component prevents even entering incorrect values by blocking out 
    incorrect keys and forcing values to stay in bounds. In the case of 
    decimal inputs (e.g. : speed), it will also block out the entry of more 
    than one decimal point.

--- src/client/components/NumberInput.vue
+++ src/client/components/NumberInput.vue
@@ -1,48 +1,133 @@
 <template>
+  <div class="input-label"> {{ label }} </div>
+
   <div class="number-container">
     <input
-      ref="number"
+      ref="input"
       type="number"
-      :min="min" :max="max" :value="value" />
-    <div class="number-arrows-container">
-      <svg viewBox="0 0 100 200">
-        <polygon
-          points="50,10 90,85 10,85"
-          @click="onUpArrowClick"/>
-        <polygon
-          points="50,190 10,115 90,115"
-          @click="onDownArrowClick"/>
-      </svg>
-    </div>
+      :min="min"
+      :max="max"
+      :value="value"
+      :step="step"
+      @keydown="onKeyDown"
+      @input="onInput"/>
   </div>
 </template>
 
 <style scoped>
-input[type="number"] {
-  pointer-events: none;
+input {
+  user-select: none;
+}
+input::selection {
+  caret-color: transparent;
+  color: black;
+  background: transparent;
+}
+.input-label {
+  margin-bottom: 8px;
 }
 .number-container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-}
-.number-arrows-container {
-  flex: 100% 0 1;
-  display: flex;
-  flex-direction: row;
-  align-items: center
 }
 </style>
 
 <script>
 export default {
-  props: [ 'min', 'max', 'value' ],
-  methods: {
-    onUpArrowClick(e) {
-      console.log('clicked up arrow');
+  props: [ 'label', 'min', 'max', 'value', 'step' ],
+  emits: ['input'],
+  computed: {
+    isDecimal() {
+      return !Number.isInteger(this.step)
     },
-    onDownArrowClick(e) {
-      console.log('clicked down arrow');
+    validInputKeys() {
+      const allValidKeys = ['0','1','2','3','4','5','6','7','8','9','Backspace','ArrowUp','ArrowDown','ArrowLeft','ArrowRight','.'];
+      return this.isDecimal ? allValidKeys : allValidKeys.slice(0,-1)
+    },
+    numberKeys() {
+      return validInputKeys.slice(0,10)
+    }
+  },
+  methods: {
+
+    // This method does three things :
+    // 1. Prevent keydown events from bubbling outside the input
+    // 2. Prevent invalid keys.
+    // 3. Ensure that at most one decimal point is ever present in the input.
+
+    onKeyDown(e) {
+      e.stopPropagation(); // 1. Prevent these inputs from bubbling and triggering the MFP
+
+      // 2. Exclude any key that doesn't make sense with the number input.
+
+      if(!this.validInputKeys.includes(e.key)) {
+        e.preventDefault();
+        return;
+      }
+
+      // 3. Prevent multiple decimal points.
+      // This is a lot of logic, but a regexp can't replace it.
+      // It would merely invalidate the field, not prevent the invalid display.
+
+      // Because we cannot rely on the input element's value, which deletes trailing decimal points
+      // (e.g. : it will interpret 1. as 1, but will STILL display 1.)
+      // we keep track of the presence of a decimal point through a flag.
+
+      const initialInputString = e.target.value
+
+      // User is deleting a digit, **or** the decimal point in the case of decimal input.
+      if(e.key === "Backspace") {
+        if(this.isDecimal && this.decimalFlag && initialInputString.length === 1) { // We are erasing the decimal point
+          // The length condition is 1 and not 2 because erasing from x.y directly produces x and not x.
+          this.decimalFlag = false;
+        }
+        else { // Ordinary deletion case
+          // We do not always delete the last character ;
+          // but if we delete anything else, trailing decimal points are not an issue.
+          const inputStringAfterBackspace = initialInputString.slice(0,-1)
+          if(inputStringAfterBackspace.endsWith('.')) this.decimalFlag = true; // Contrary to the Number conversion, this *can* end with "."
+        }
+      }
+
+      // User is inputting the decimal point themselves
+      // The value doesn't actually change until they enter a decimal,
+      // Which means that trailing decimal points (like 1., 2.) can exist.
+      else if(e.key === "." && this.isDecimal) {
+        if(initialInputString.length === 1 && !this.decimalFlag) this.decimalFlag = true; // This means : a solitary integer without a trailing decimal (e.g. excluding 2.)
+        else e.preventDefault() // Cut the event line immediately if there would be multiple decimal points.
+      }
+
+      // One edge case is still not covered, however, and to examinate it, these two variables must be persisted.
+      // They will be used in the input listener.
+
+      this.lastKeyPressed = e.key
+      this.valueOnKeyDown = e.target.valueAsNumber
+    },
+    onInput(e) {
+
+      // Decimal point compliance edge case :
+      // Ensure that scrolling up or down (which never produces trailing decimal points like 1.)
+      // will reset the decimal flag.
+      // This case is never triggered if the input is not decimal (because the value will always be an integer).
+
+      if(
+        (this.lastKeyPressed === "ArrowDown" || this.lastKeyPressed === "ArrowUp")
+        && !Number.isInteger(this.valueOnKeyDown)
+        && Number.isInteger(e.target.valueAsNumber)
+      ) this.decimalFlag = false
+
+      // We need to ensure the value displayed to the user stays in bounds
+      // This is not enforced by min and max.
+      // It's MUCH better to do it here than on keydown (because we have the actual value of the input)
+
+      if(e.target.value < this.min) e.target.value = this.min
+      if(e.target.value > this.max) e.target.value = this.max
+
+      this.$emit('input',e)
+    },
+    reset() {
+      this.$refs.input.value = this.value
     }
   },
 };
--- src/client/components/ScrollBar.vue
+++ src/client/components/ScrollBar.vue
@@ -83,66 +83,51 @@
     </div>
 
     <div v-if="hasBounds" class="speed-input">
-      <div class="input-label"> {{ $t('scrollBar.speed') }} </div>
-
-      <input
+      <NumberInput
         ref="speed-input"
-        name="speed"
-        type="number"
+        :label="$t('scrollBar.speed')"
         :min="0.1"
         :max="10"
         :step="0.1"
         :value="1"
-        @keydown="onInputKeyDown"
         @input="onPlaybackSpeedChanged"
       />
     </div>
 
     <div v-if="hasBounds">
-      <div class="input-label"> {{$t('scrollBar.start')}} </div>
-      <!-- <div class="event-number"> {{ start + 1 }} </div> -->
-      <input
+      <NumberInput
         ref="start-input"
-        name="start"
-        type="number"
+        :label="$t('scrollBar.start')"
         :min="1"
         :max="end + 1"
+        :step="1"
         :value="start + 1"
-        @keydown="onInputKeyDown"
-        @input="onStartInput" />
+        @input="onStartInput"
+      />
     </div>
+
     <div :class="!hasBounds ? 'no-padding-indice' : ''">
-      <div class="input-label"> {{ capitalizedIndexLabel || $t('scrollBar.current') }} </div>
-      <!-- <div class="event-number"> {{ index + 1 }} </div> -->
-      <input
+      <NumberInput
         ref="index-input"
-        name="index"
-        type="number"
+        :label="capitalizedIndexLabel || $t('scrollBar.current')"
         :min="hasBounds ? start + 1 : start"
         :max="hasBounds ? end + 1 : end"
+        :step="1"
         :value="hasBounds ? Math.max(index + 1, start + 1) : Math.max(index, start)"
-        @keydown="onInputKeyDown"
-        @input="onIndexInput" />
-      <!-- <number-input
-        ref="index-input"
-        :min="(start + 1)"
-        :max="(end + 1 )"
-        :value="(Math.max(index + 1, start + 1))"
-        @change="onIndexChange"/> -->
+        @input="onIndexInput"
+      />
     </div>
+
     <div v-if="hasBounds">
-      <div class="input-label"> {{$t('scrollBar.end')}} </div>
-      <!-- <div class="event-number"> {{ end + 1 }} </div> -->
-      <input
+      <NumberInput
         ref="end-input"
-        name="end"
-        type="number"
+        :label="$t('scrollBar.end')"
         :min="start + 1"
         :max="Math.max(1, size)"
+        :step="1"
         :value="end + 1"
-        @keydown="onInputKeyDown"
-        @input="onEndInput" />
-        <!-- @change="onEndChange" /> -->
+        @input="onEndInput"
+      />
     </div>
 
     <div v-if="hasBounds" class="stop-button-container" @click="silence()">
@@ -251,18 +236,6 @@ rect, circle {
   padding-top: 0;
   padding-bottom: 0;
 }
-.indices input[type="number"] {
-  /* pointer-events: none; */
-  user-select: none;
-}
-.indices input[type="number"]::selection {
-  caret-color: transparent;
-  color: black;
-  background: transparent;
-}
-.indices .input-label {
-  margin-bottom: 8px;
-}
 .event-number {
   font-size: 2em;
 }
@@ -353,30 +326,24 @@ export default {
   },
   methods: {
     onStartInput(e) {
-      const input = this.$refs['start-input'];
-      let v = input.value - 1;
+      let v = e.target.value - 1;
       v = Math.min(this.end, Math.max(0, v));
-      // input.value = v;
       if(this.hasBounds) // this method shouldn't be called if this is false anyway, but just to be sure
         this.$emit('start', v);
       if (this.index < v)
         this.$emit('index', v);
     },
     onIndexInput(e) {
-      const input = this.$refs['index-input'];
-      let v = this.hasBounds ? input.value - 1 : input.value;
+      let v = this.hasBounds ? e.target.value - 1 : e.target.value;
       v = Math.min(this.end, Math.max(this.start, v));
-      // input.value = v;
       this.$emit('index', v);
     },
     onIndexChange(e) {
 
     },
     onEndInput(e) {
-      const input = this.$refs['end-input'];
-      let v = input.value - 1;
+      let v = e.target.value - 1;
       v = Math.min(this.size - 1, Math.max(this.start, v));
-      // input.value = v;
       if(this.hasBounds) // this method shouldn't be called if this is false anyway, but just to be sure
         this.$emit('end', v);
       if (this.index > v)
@@ -442,8 +409,7 @@ export default {
       this.$emit('modeChange', newMode);
     },
     onPlaybackSpeedChanged(e) {
-      const input = this.$refs['speed-input']
-      this.$emit('speed', input.value)
+      this.$emit('speed', e.target.value)
     },
     onKeyDown(e) {
       if(this.hasBounds) { // I don't think most users will want to use the arrow keys for velocities or playback speed, so this avoids having to manage focus
@@ -451,91 +417,10 @@ export default {
         if(e.code==="ArrowRight") this.$emit("index",this.index + 1)
       }
     },
-    // BEWARE : extremely messy code ahead
-    // Even a regexp would not properly handle every case...
-    // There has to be a better way to do this, right ???
-    onInputKeyDown(e) {
-      e.stopPropagation(); // prevent these inputs from triggering the MFP
-
-      // This was the simple part.
-      // Now, we need to ensure the value displayed to the user stays in bounds
-      // This is not enforced by min and max.
-
-      // First : we want to exclude any key that doesn't make sense with the number input.
-
-      const allValidKeys = ['0','1','2','3','4','5','6','7','8','9','Backspace','ArrowUp','ArrowDown','.']
-      const numberKeys = allValidKeys.slice(0,10)
-
-      let validInputKeys;
-      if(e.target.name === "speed") validInputKeys = allValidKeys // Speed takes decimals
-      else validInputKeys = allValidKeys.slice(0,-1)
-
-      // Then, we must determine what the tentative value after the event would be.
-
-      let attemptedValue;
-
-      // Disclaimer : I think using Number conversion on e.target.value instead of valueAsNumber helps with uniformity and readability here.
-      // Else ifs instead of switch is to avoid having to fallthrough for all numerical cases
-
-      const initialInputString = e.target.value
-      const initialInputNumber = Number(initialInputString)
-
-      if(numberKeys.includes(e.key)) { // User pressed a number key. Simply concatenate.
-        // The decimal flag exists to distinguish between "1" and "1.",
-        // which e.target.value does not do.
-        if(initialInputString.includes('.') || (this.decimalFlag && e.target.name === "speed")) {
-          // null check necessary or we get a NaN
-          // yes, even with the safe call operator
-          const decimalPlaces = initialInputString.split('.')[1] ? initialInputString.split('.')[1].length : 0
-          attemptedValue = initialInputNumber + (Number(e.key) * 10**-(decimalPlaces+1))
-        }
-        else attemptedValue = Number(e.target.value+e.key)
-      }
-
-      // User is incrementing by the step.
-      else if(e.key === "ArrowUp") attemptedValue = initialInputNumber + Number(e.target.step)
-      else if(e.key === "ArrowDown") attemptedValue = initialInputNumber - Number(e.target.step)
-
-      // User is deleting a digit, **or** the decimal point in the case of speed input.
-      else if(e.key === "Backspace") {
-        if(this.decimalFlag && e.target.name === "speed" && initialInputString.length === 1) { // We are erasing the decimal point
-          // The length condition is 1 and not 2 because erasing from x.y directly produces x and not x.
-          attemptedValue = initialInputNumber
-          this.decimalFlag = false;
-        }
-        else { // Ordinary deletion case
-          const inputStringAfterBackspace = initialInputString.slice(0,-1)
-          attemptedValue = Number(inputStringAfterBackspace)
-          if(inputStringAfterBackspace.endsWith('.')) this.decimalFlag = true; // Contrary to the Number conversion, this *can* end with "."
-        }
-      }
-
-      // User is inputting the decimal point themselves (for the speed)
-      // The value doesn't actually change until they enter a decimal.
-      // Of course, we can't have multiple decimal points.
-      else if(e.key === "." && e.target.name === "speed" && !initialInputString.includes('.') && !this.decimalFlag) {
-        attemptedValue = initialInputNumber
-        this.decimalFlag = true;
-      }
-
-      // ...none of these checks are made by the input.
-
-      const parsedMin = Number(e.target.min);
-      const parsedMax = Number(e.target.max);
-
-      // All of this case checking was for this single action :
-      // If the tentative value set by the user is not in bounds, reject the event.
-
-      if(!validInputKeys.includes(e.key)
-        || (attemptedValue < parsedMin || attemptedValue > parsedMax)) {
-          e.preventDefault();
-          // TODO : allow attempted values out of bounds but clip them back in.
-          // This means modifying the event value. Is this doable ?
-        }
-    },
-    resetSpeedDisplay() { // FIXME : REPLACE THIS WITH REACTIVITY BY CHANGING THE COMPONENT STRUCTURE
+    resetSpeedDisplay() {
+      // FIXME : REPLACE THIS WITH REACTIVITY BY CHANGING THE COMPONENT STRUCTURE
       // (divorce ScrollBar from Slider, and give ScrollBar a "speed" prop)
-      this.$refs['speed-input'].value = 1
+      this.$refs['speed-input'].reset()
     },
     silence() {
       this.$emit('silence')
