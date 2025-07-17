f99a7411d3c8a3fd37322685082c3517532c4ff3
Joseph Larralde <joseph.larralde@gmail.com>
2025-07-17 11:17:44 +0200

    pass baseUrl and allow use of vueDevTools

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  optimizeDeps: {
-    exclude: [
-      "midifile-performer",
-    ]
-  }
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    // appType: "mpa",x
+    plugins: [
+      vue(),
+      // vueDevTools(),
+    ],
+    base: env.VITE_BASE_URL || "/",
+    optimizeDeps: {
+      exclude: [
+        "midifile-performer",
+      ]
+    }
+  });
+};
